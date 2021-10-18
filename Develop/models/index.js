3f5f6bf65fd86c2cedb3f8ab778ceda4804a0e38
Daniel O <danielolvera2188@gmail.com>
2021-10-17 23:30:30 -0600

    defined columns of all the models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsToMany(Category, {
 
+})
 // Categories have many Products
+Category.hasMany(Product, {
 
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tags, {
+  through: ProductTag
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag
+})
 module.exports = {
   Product,
   Category,
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# E-commerce Back End Starter Code
+# E-commerce Back End
+
+## Description
+
+Using Sequelize and a MySQL database, build the back end for an e-commerce site.
+
+## Technologies Used
+
+- MySQL2
+- Sequelize
+- dotenv
+
+## Screenshots
+
+# Links
+
+Github Repo: https://github.com/danielolvera21/e-commerce-backend
+
+Walkthrough video:
